4154dec27d2bd90d6879b8633c3b4817c10e33f3
Programador Full Stack 2024.1 <16133232024.1@senacrjedu.local>
2025-02-25 21:40:35 -0300

    encapsulamento

--- src/App.java
+++ src/App.java
@@ -4,10 +4,13 @@ import domain.model.Pessoa;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
+       
+        try {        
+       
 
         Agencia agencia = new Agencia();
-        Pessoa pessoa = new Pessoa();
+        Pessoa pessoa = new Pessoa("Anderson","andymarques1@email.com","+55 (21) 4444-55555");
         Conta conta = new Conta();
         
 
@@ -16,19 +19,26 @@ public class App {
         agencia.email = "campogrande@bankweb.com";
         agencia.tel = "+55 (21) 555-55555";
 
-        pessoa.nome = "Anderson Marques";
-        pessoa.cpf = "111.222.333-44";
-        pessoa.tel = "+55 (21) 4444-55555";
-        pessoa.email = "andymarques1@email.com";
+        // pessoa.setNome("Anderson");
+        pessoa.setCpf("111.222.333-44");
+        pessoa.setCnpj("34.283.131.0001-45");
+        pessoa.setDataNasc("01/01/1111");
+        // pessoa.setTel("+55 (21) 4444-55555");
+        // pessoa.setEmail("andymarques1@email.com");
+        
 
         conta.numero = "100001";
         conta.agencia = agencia;
         conta.pessoa = pessoa;
+        conta.saldo = 1000;
 
         System.out.println("Agencia: "+ conta.agencia.nome);
-        System.out.println("Cliente: "+ conta.pessoa.nome);
+        System.out.println("Cliente: "+ conta.pessoa.getNome());
         System.out.println("Saldo: "+ conta.saldo);
 
+    } catch (Exception e) {
+        System.err.println(e.getMessage());
+    }
 
     }
 }
--- src/domain/model/Pessoa.java
+++ src/domain/model/Pessoa.java
@@ -2,13 +2,76 @@ package domain.model;
 
 public class Pessoa {
 
-    public String nome;
-    public String cpf;
-    public String cnpj;
-    public String dataNasc;
-    public String email;
-    public String tel;
+    private String nome;
+    private String cpf;
+    private String cnpj;
+    private String dataNasc;
+    private String email;
+    private String tel;
 
+    // NOME
+
+    public Pessoa(String nome, String email, String Tel) throws Exception {
+        this.setNome(nome);  //   this.setNome = nome
+        this.setEmail(email);  // this.setEmail = email
+        this.setTel(Tel);  //  this.setTel = Tel
+
+    }
+
+    public void setNome (String nome) throws Exception {
+        if (nome.trim().equals(""))
+            throw new Exception("Nome em Branco!");
+        this.nome = nome.trim();
+    }
+    public String getNome(){
+        return this.nome;
+    }
+
+    // CPF
+
+    public void setCpf (String cpf){
+            this.cpf = cpf;
+    }
+    public String getCpf(){
+        return this.cpf;
+    }
+
+    // CNPJ
+
+    public void setCnpj (String cnpj){
+        this.cnpj = cnpj;
+    }
+    public String getCnpj(){
+    return this.cnpj;
+    }
+
+    // DATA DE NASCIMENTO
+
+    public void setDataNasc (String dataNasc){
+        this.dataNasc = dataNasc;
+    }
+    public String getDataNasc(){
+    return this.dataNasc;
+    }
+
+    // EMAIL
+
+    public void setEmail (String email){
+        this.email = email;
+    }
+    public String getEmail(){
+    return this.email;
+    }
+
+    // TELEFONE
+
+    public void setTel (String tel){
+        this.tel = tel;
+    }
+    public String getTel(){
+    return this.tel;
+    }
+    
 }
 
 
